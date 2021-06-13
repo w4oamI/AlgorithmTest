ce170b8efd9f3341e5383d05173801221749b3b6
EcoSys <>
2021-06-13 23:36:36 +0000

    (문자열과 튜플)(사전, 집합 자료형)

--- grammar/float.py
+++ grammar/float.py
@@ -21,3 +21,28 @@ print(a)
 #실수를 정수화
 a = int(17.5)
 print(a)
+
+
+#round()함수 이용
+print("round()함수 이용X")
+#----------------------#
+#실수 덧셈으로 조건문 힘들다.
+a = 0.3 + 0.6
+print(a)
+
+if a==0.9:
+  print(True)
+else:
+  print(False)
+#False가 나온다.
+#그렇기 때문에 round()함수를 사용.
+print("round()함수 이용")
+a = 0.3+0.6
+print(a)
+print(round(a,1))
+
+if round(a,1)==0.9:
+  print(True)
+else:
+  print(False)
+#----------------------#
---
+++ grammar/string.py
@@ -0,0 +1,25 @@
+data = "hello world"
+print(data)
+
+data = "Don't \"human\"?"
+print(data)
+
+#문자열도 인덱싱과 슬라이싱이 된다.
+#하지만 인덱스의 값은 "변경"이 안된다.
+
+#문자열 연산
+#----------------------------------#
+a = 'hello'
+b = 'world'
+#문자열 덧셈
+print(a +" "+ b) #hello world
+
+#문자열 곱셈
+print(a*2)  #hellohello
+
+#문자열 슬라이싱
+print(a[0:4]) #hell
+
+#문자열 인덱싱
+print(a[4]) #o
+#----------------------------------#
